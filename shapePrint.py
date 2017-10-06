5eb53bcaa10afab535a673bddcf629cc5c2e9941
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2017-10-06 13:37:47 +0800

    initial commit

---
+++ shapePrint.py
@@ -0,0 +1,68 @@
+import sys
+def drawSquare(side):
+	############################################################################
+	# TODO: Implement this code segment                                        #
+	############################################################################
+	pass
+	############################################################################
+	# END CODE HERE					                                           #
+	############################################################################
+
+def drawRectangle(length,width):
+	for i in range(0,length):
+		for y in range(0,width):
+			sys.stdout.write("*")
+		print("")
+
+def drawRightTriangle(height):
+	############################################################################
+	# TODO: Implement this code segment                                        #
+	############################################################################
+	pass
+	############################################################################
+	# END CODE HERE					                                           #
+	############################################################################
+
+def drawDiamond(height):
+	############################################################################
+	# TODO: Implement this code segment                                        #
+	############################################################################
+	pass
+	############################################################################
+	# END CODE HERE					                                           #
+	############################################################################
+
+print("----------------------------Shape Printer----------------------------")
+while True:
+	resp = 0
+	while True:
+		print("Which shape do you want to draw?")
+		print("[1] - Square")
+		print("[2] - Rectangle")
+		print("[3] - Right Triangle")
+		print("[4] - Diamond")
+		print("[5] - Exit")
+		try:
+			resp = int(input("Response: "))
+			if resp >= 1 and resp <= 5:
+				break
+			else:
+				print("Response must be from 1 to 5")
+		except ValueError:
+			print("Input must be a number")
+
+	if resp == 1:
+		side = int(input("Enter side length: "))
+		drawSquare(side)
+	elif resp == 2:
+		length = int(input("Enter length: "))
+		width = int(input("Enter width: "))
+		drawRectangle(length,width)
+	elif resp == 3:
+		height = int(input("Enter height: "))
+		drawRightTriangle(height)
+	elif resp == 4:
+		height = int(input("Enter height: "))
+		drawDiamond(height)
+	elif resp == 5:
+		break
